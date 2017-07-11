71348870066c519631e67d5fdcc36fec87e0b273
moveordie <moveordie@gmail.com>
2017-07-11 23:23:35 +0300

    Added login page, and login logic, login successful page is still missing.

--- WebApplication1/Controllers/LoginController.cs
+++ WebApplication1/Controllers/LoginController.cs
@@ -13,26 +13,52 @@ namespace WebApplication1.Controllers
         // GET: Login
         public ActionResult Login()
         {
-            User u = new User();
+            return View(new User());
+        }
+
+        public ActionResult Login(User u)
+        {
+            return View("PostLogin", TryLogin(u));
+        }
+
+        public ActionResult PostLogin(User u)
+        {
+            if (u == null)
+            {
+                return View("Login", new User());
+            }
+
             return View(u);
         }
 
-        public ActionResult LoginSuccessful(User u)
+        // Takes user with 'UserName' and 'UserPassword'.
+        // Returns the full user from the database if exists, or null otherwise.
+        public User TryLogin(User u)
         {
-            // If u exists in db:
-            //    return OK
-            // else:
-            //    return error
-
             UserDal ud = new UserDal();
-
             List<User> users = (
+                // For each user in ud.Users (call it user_from_db),
+                // If it's username and password fields equals to those of u:
+                // select: user_from_db.
                 from user_from_db in ud.Users
                 where user_from_db.UserName == u.UserName && user_from_db.UserPassword == u.UserPassword
                 select user_from_db
             ).ToList<User>();
 
+            //list<user> result;
+            //for (int i = 0; i < ud.users.length; i++)
+            //{
+            //    user user_from_db = ud.users[i];
+            //    if (user_from_db.username == u.username && u.userpassword == user_from_db.userpassword)
+            //    {
+            //        result.add(user_from_db);
+            //    }
 
+            if (users.Count() == 0)
+            {
+                return null;
+            }
+            return users[0];
         }
     }
 }
