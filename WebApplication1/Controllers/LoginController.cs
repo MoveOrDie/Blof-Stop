f11fb60e9c5ee75c07fc28647067683e61bba617
moveordie <moveordie@gmail.com>
2017-07-11 22:51:00 +0300

    hgjghj

--- WebApplication1/Controllers/LoginController.cs
+++ WebApplication1/Controllers/LoginController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.Dal;
 using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
@@ -14,6 +15,24 @@ namespace WebApplication1.Controllers
         {
             User u = new User();
             return View(u);
+        }
+
+        public ActionResult LoginSuccessful(User u)
+        {
+            // If u exists in db:
+            //    return OK
+            // else:
+            //    return error
+
+            UserDal ud = new UserDal();
+
+            List<User> users = (
+                from user_from_db in ud.Users
+                where user_from_db.UserName == u.UserName && user_from_db.UserPassword == u.UserPassword
+                select user_from_db
+            ).ToList<User>();
+
+
         }
     }
 }
